077c1d73340e1e482a8a21f5ad87db7cf96d8ebc
mergey <33480225+mergey@users.noreply.github.com>
2018-02-22 12:34:46 +0100

    now sending HTTP POST request

--- CYLweather.ts
+++ CYLweather.ts
@@ -1,24 +1,21 @@
 
 /**
- * Paket um den Wetter-Dienst von Code your Life anzusprechen.
+ * Paket von Code your Life
  * @author Marian Mehling
  * @version 0.3
  */
 
+
 /**
  * Befehle für das senden von Wetterdaten an den Code your Life Wetter-Server.
  */
-//% weight=2 color=#b4009f icon="\uf1eb" block="Code your Life"
-//% parts="Code your Life"
-namespace cyl {
-
-    const wUrl = "momatita.kleeberg.berlin"
-    const port = 3001
+//% weight=100 color=#b4009f icon="\uf1eb" block="Code your Life"
+namespace custom {
 
     /**
      * Mit diesem Block können enventuell auftretende Fehler behoben werden.
      */
-    //% blockId=cyl_resetWifi block="das WLAN-Modul zurücksetzen"
+    //% blockId=custom_resetWifi block="das WLAN-Modul zurücksetzen"
     export function resetWifi(): void {
         basic.clearScreen()
         led.plot(0, 0)
@@ -27,9 +24,9 @@ namespace cyl {
             SerialPin.C16,
             BaudRate.BaudRate9600
         )
-        modem.pushAT("+UART=115200,8,1,0,0")
+        modem.pushAT('+UART=115200,8,1,0,0')
         basic.pause(500)
-        modem.pushAT("+RST")
+        modem.pushAT('+RST')
         basic.pause(2000)
         led.plot(1, 0)
         modem.init(
@@ -37,9 +34,9 @@ namespace cyl {
             SerialPin.C16,
             BaudRate.BaudRate56700
         )
-        modem.pushAT("+UART=115200,8,1,0,0")
+        modem.pushAT('+UART=115200,8,1,0,0')
         basic.pause(500)
-        modem.pushAT("+RST")
+        modem.pushAT('+RST')
         basic.pause(2000)
         led.plot(2, 0)
         modem.init(
@@ -47,18 +44,18 @@ namespace cyl {
             SerialPin.C16,
             BaudRate.BaudRate115200
         )
-        modem.pushAT("+CWJAP=\"namexX00\",\"passwordxX00\"")
+        modem.pushAT('+CWJAP="namexX00","passwordxX00"')
         basic.pause(3000)
         led.plot(3, 0)
         basic.pause(2000)
-        modem.pushAT("+RST")
+        modem.pushAT('+RST')
         led.plot(4, 0)
     }
 
     /**
      * Mit diesem Block wird die Verbindung zu einem "Code your Life" WLAN-Modul hergestellt.
      */
-    //% blockId=cyl_initWifi block="mit dem WLAN-Modul verbinden"
+    //% blockId=custom_initWifi block="mit dem WLAN-Modul verbinden"
     export function initWifi(): void {
         modem.init(
             SerialPin.C17,
@@ -70,7 +67,7 @@ namespace cyl {
         serial.setReceiveBufferSize(254)
         // clear buffer
         serial.readString();
-        modem.expectOK("E0");
+        modem.expectOK('E0');
     }
 
     /**
@@ -78,12 +75,16 @@ namespace cyl {
      * @param name Name des WLAN-Netzwerkes, eg: "Name des WLAN"
      * @param pass Passwort des WLAN-Netzwerkes, eg: "Passwort des WLAN"
      */
-    //% blockId=cyl_connectWifi block="mit WLAN verbinden:|Name %name|Passwort %pass"
+    //% blockId=custom_connectWifi block="mit WLAN verbinden:|Name %name|Passwort %pass"
     export function connectWifi(name: string, pass: string): void {
         let repeat = true
+
         while (repeat) {
-            if (modem.expectOK("+CWMODE=1")) {
-                modem.pushAT("+CWJAP=\"" + name + "\",\"" + pass + "\"")
+            let data = modem.sendAT('+CWJAP?')
+            if (data.length >= 2 && data[data.length - 2].compare('No AP') != 0 && data[data.length - 1].compare('OK') == 0) {
+                repeat = false
+            } else if (modem.expectOK('+CWMODE=1')) {
+                modem.pushAT('+CWJAP="' + name + '","' + pass + '"')
                 repeat = false
                 basic.pause(10000)
             }
@@ -100,21 +101,22 @@ namespace cyl {
      * @param wind Windstärke die gesendet werden soll, eg: 0
      * @param rain Niederschlagshöhe die gesendet werden soll, eg: 0
      */
-    //% blockId=cyl_sendData block="Wetterdaten an den Server senden|Wetter-ID %key|Temperatur %temp|Helligkeit %light|Windstärke %wind|Niederschlag %rain"
+    //% blockId=custom_sendData block="Wetterdaten an den Server senden|Wetter-ID %key|Temperatur %temp|Helligkeit %light|Windstärke %wind|Niederschlag %rain"
     export function sendData(key: string, temp: number, light: number, wind: number, rain: number): void {
-        let message = "{ \"key\": \"" + key + "\", \"temp\": " + temp + ", \"light\": " + light + ", \"wind\": " + wind + ", \"rain\": " + rain + " }"
+        const url = 'momatita.kleeberg.berlin'
+        const port = 80
+        
+        let data = '{ "key": "' + key + '", "temp": ' + temp + ', "light": ' + light + ', "wind": ' + wind + ', "rain": ' + rain + ' }'
+        let msg = 'POST /upload HTTP/1.1\r\nHost: ' + url + '\r\nContent-Type: application/json \r\nContent-Length: ' + data.length + '\r\n\r\n' + data + '\r\n\r\n';
 
-        let fail = true
-        while (fail) {
-            if (modem.expectOK("+CIPMODE=0")) {
-                if (modem.expectOK("+CIPSTART=\"TCP\",\"" + wUrl + "\"," + port + "")) {
-                    basic.pause(500)
-                    modem.pushAT("+CIPSEND=" + message.length)
-                    basic.pause(500)
-                    serial.writeString(message)
-                    basic.pause(500)
-                    fail = !(modem.expectOK("+CIPCLOSE"))
-                }
+        if (modem.expectOK('+CIPMODE=0')) {
+            modem.pushAT('+CIPMUX=1');
+            if (modem.expectOK('+CIPSTART=4,"TCP","' + url + '",' + port)) {
+                basic.pause(500)
+                modem.pushAT('+CIPSEND=4,' + msg.length)
+                basic.pause(500)
+                serial.writeString(msg)
+                basic.pause(500)
             }
         }
     }
